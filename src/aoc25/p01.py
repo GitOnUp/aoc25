1f414a3c89e101a05063d9a8d64391ce7bc67174
George Murray <george@summation.com>
2025-12-04 15:41:09 -0800

    p01 initial

---
+++ src/aoc25/p01.py
@@ -0,0 +1,56 @@
+from os import PathLike
+from pathlib import Path
+
+
+def solve(input_file: PathLike):
+    current = 50
+    total_zero = 0
+    with open(input_file) as f:
+        for line in f:
+            if not line:
+                break
+            direction = line[0]
+            count = int(line[1:])
+            if direction == "R":
+                current += count
+            else:
+                current -= count
+
+            if (current % 100) == 0:
+                total_zero += 1
+    return total_zero
+
+
+def solve2(input_file: PathLike):
+    current = 50
+    total_zero = 0
+    with open(input_file) as f:
+        for line in f:
+            if not line:
+                break
+            direction = line[0]
+            count = int(line[1:])
+            full_turns = abs(count // 100)
+            total_zero += full_turns
+            count -= full_turns * 100
+            previous = current
+            if direction == "R":
+                current += count
+            else:
+                current -= count
+            if current < 0:
+                current += 100
+                if previous != 0:
+                    total_zero += 1
+            elif current >= 100:
+                current -= 100
+                if previous != 0:
+                    total_zero += 1
+            elif current == 0:
+                total_zero += 1
+    return total_zero
+
+
+if __name__ =="__main__":
+    print(solve(Path(__file__).parent.parent / "input" / "p01.txt"))
+    print(solve2(Path(__file__).parent.parent / "input" / "p01.txt"))
