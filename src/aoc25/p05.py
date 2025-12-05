56de107d197a03c56821c554a06d79b8d926969f
George Murray <george@summation.com>
2025-12-05 13:50:38 -0800

    Day 5 part 2

--- src/aoc25/p05.py
+++ src/aoc25/p05.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import aoc25.util as util
 
 
-def solve(input_file: Path) -> (int, int):
+def solve(input_file: Path) -> tuple[int, int]:
     ranges = []
 
     fresh_count = 0
@@ -17,7 +17,7 @@ def solve(input_file: Path) -> (int, int):
 
             if in_ranges:
                 low, high = (int(i) for i in line.split('-'))
-                ranges.append((low, high))
+                ranges = merge_range(ranges, (low, high))
                 continue
 
             num = int(line)
@@ -25,30 +25,38 @@ def solve(input_file: Path) -> (int, int):
                 if test_range[0] <= num <= test_range[1]:
                     fresh_count += 1
                     break
+    print(ranges)
     return fresh_count, count_fresh(ranges)
 
 
 def count_fresh(ranges: list[tuple[int, int]]) -> int:
-    ranges = list(sorted(ranges, key=lambda x: x[0]))
-    print(ranges)
-    compact_ranges = []
-    i = 0
-    while i < len(ranges):
-        current_range = ranges[i]
-        i_next = i + 1
-        while i_next < len(ranges):
-            if current_range[1] < ranges[i_next][0]:
-                break
-            current_range = (current_range[0], ranges[i_next][1])
-            i_next += 1
-        compact_ranges.append(current_range)
-        i = i_next
     fresh = 0
-    print(compact_ranges)
-    for r in compact_ranges:
+    for r in ranges:
         fresh += r[1] - r[0] + 1
     return fresh
 
 
+def merge_range(ranges: list[tuple[int, int]], new_range: tuple[int, int]) -> list[tuple[int, int]]:
+    merged_ranges = []
+    removed = []
+    for i, range in enumerate(ranges):
+        if (
+                range[0] <= new_range[0] <= range[1]
+                or range[0] <= new_range[1] <= range[1]
+                or new_range[0] <= range[0] <= new_range[1]
+                or new_range[0] <= range[1] <= new_range[1]
+        ):
+            removed.append(i)
+    min_range, max_range = new_range
+    for i, range in enumerate(ranges):
+        if i not in removed:
+            merged_ranges.append(range)
+            continue
+        min_range = min(min_range, range[0])
+        max_range = max(max_range, range[1])
+    merged_ranges.append((min_range, max_range))
+    return merged_ranges
+
+
 if __name__ == "__main__":
-    print(solve(util.input_path("p05.example")))
+    print(solve(util.input_path("p05.txt")))
