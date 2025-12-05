dfabdb282c6aa69a87d567cda98c9b8cc87e7f56
George Murray <george@summation.com>
2025-12-04 17:50:49 -0800

    Day 3

---
+++ src/aoc25/p03.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+import aoc25.util as util
+
+
+def largest_joltage_2(bank: str) -> int:
+    largest_past = [0] * len(bank)
+    largest = 0
+    for i in range(len(largest_past) - 2, -1, -1):
+        largest = max(largest, int(bank[i+1]))
+        largest_past[i] = largest
+    for i in range(0, len(bank) - 1):
+        check = int(bank[i]) * 10 + largest_past[i]
+        largest = max(largest, check)
+    return largest
+
+
+def largest_joltage_iter(bank: list[int], num: int, concat: str) -> str | None:
+    if num == 0:
+        return concat
+    if len(bank) < num:
+        return None
+    for digit in range(9, 0, -1):
+        for i, bank_digit in enumerate(bank):
+            if bank_digit == digit:
+                check = largest_joltage_iter(bank[i+1:], num-1, concat + str(digit))
+                if check is not None:
+                    return check
+                else:
+                    break
+    return None
+
+
+def solve(input_file: Path):
+    with open(input_file) as f:
+        total = 0
+        total_12 = 0
+        for bank in f:
+            total += largest_joltage_2(bank.strip())
+            total_12 += int(largest_joltage_iter([int(c) for c in bank.strip()], 12, ""))
+    return total, total_12
+
+
+if __name__ == "__main__":
+    print(solve(util.input_path("p03.txt")))
