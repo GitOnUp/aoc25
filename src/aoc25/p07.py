327efe5390ff7f52dcb5da893ea9c681fa3a2c3c
George Murray <murray.george.m@gmail.com>
2025-12-06 22:00:27 -0800

    Day 7 p1

--- src/aoc25/p07.py
+++ src/aoc25/p07.py
@@ -4,8 +4,30 @@ from aoc25 import util
 
 
 def solve(input_file: Path):
-    pass
+    with open(input_file, "r") as f:
+        lines = [line.strip() for line in f.readlines()]
+
+    s = -1
+    for i, c in enumerate(lines[0]):
+        if c == "S":
+            s = i
+
+    beams = {s}
+    splits = 0
+    for line in lines[1:]:
+        new_beams = set()
+        for beam in beams:
+            if line[beam] == ".":
+                new_beams.add(beam)
+                continue
+            splits += 1
+            if beam > 0:
+                new_beams.add(beam-1)
+            if beam < len(line) - 1:
+                new_beams.add(beam+1)
+        beams = new_beams
+    return splits
 
 
 if __name__ == "__main__":
-    print(solve(util.input_path(__file__, example=True)))
+    print(solve(util.input_path(__file__, example=False)))
