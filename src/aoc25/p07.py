f8091d0b7d97720ae6364784e57f9a22e3d426eb
George Murray <murray.george.m@gmail.com>
2025-12-06 22:34:27 -0800

    Day 7 part 2

--- src/aoc25/p07.py
+++ src/aoc25/p07.py
@@ -1,9 +1,10 @@
+from functools import cache
 from pathlib import Path
 
 from aoc25 import util
 
 
-def solve(input_file: Path):
+def solve(input_file: Path) -> tuple[int, int]:
     with open(input_file, "r") as f:
         lines = [line.strip() for line in f.readlines()]
 
@@ -12,6 +13,10 @@ def solve(input_file: Path):
         if c == "S":
             s = i
 
+    return solve1(s, lines), solve2(s, lines)
+
+
+def solve1(s: int, lines: list[str]) -> int:
     beams = {s}
     splits = 0
     for line in lines[1:]:
@@ -27,6 +32,18 @@ def solve(input_file: Path):
                 new_beams.add(beam+1)
         beams = new_beams
     return splits
+
+
+def solve2(beam: int, lines: list[str]) -> int:
+    @cache
+    def recurse(b: int, line_ix: int) -> int:
+        if line_ix >= len(lines):
+            return 1
+        if lines[line_ix][b] == ".":
+            return recurse(b, line_ix+1)
+        return recurse(b+1, line_ix+1) + recurse(b-1, line_ix+1)
+
+    return recurse(beam, 1)
 
 
 if __name__ == "__main__":
